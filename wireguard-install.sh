93ea133bae7b172022684e1cf62d3ec655fbcc7a
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-02-14 22:34:49 -0500

    Update wireguard-install.sh

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -37,7 +37,7 @@ if [ ! -f "$WG_CONFIG" ]; then
     GATEWAY_ADDRESS="${PRIVATE_SUBNET::-4}1"
 
     if [ "$SERVER_HOST" == "" ]; then
-        SERVER_HOST="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+        SERVER_HOST="$(dig -4 +short o-o.myaddr.l.google.com @ns3.google.com)"
         if [ "$INTERACTIVE" == "yes" ]; then
             read -p "Servers public IP address is $SERVER_HOST. Is that correct? [y/n]: " -e -i "y" CONFIRM
             if [ "$CONFIRM" == "n" ]; then
